0176ed77a940eda76df5dd1798a6edb1bae8ff46
Anuraggg-19 <bondeanurag78@gmail.com>
2026-07-20 15:36:02 +0530

    Neo4j data fetching script

--- app/neo4j_client.py
+++ app/neo4j_client.py
@@ -21,7 +21,7 @@ from neo4j import GraphDatabase
 
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "Neo4j123")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "")
 
 _driver: Optional[GraphDatabase.driver] = None
 
